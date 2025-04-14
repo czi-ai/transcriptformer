f357e88b9a074d503862fe4fff957cf3bb4cf424
flow <flow@chanzuckerberg.com>
2025-04-14 10:23:11 -0700

    add datasets

---
+++ src/transcriptformer/datasets.py
@@ -0,0 +1,50 @@
+import scanpy as sc
+import anndata as ad
+from cellxgene_census import download_source_h5ad
+import os
+from typing import Any, Literal, Tuple, Union
+from scanpy.readwrite import _check_datafile_present_and_download
+
+PathLike = Union[os.PathLike, str]
+
+def lymphnode_tsv2(
+    path: PathLike = "~/.cache/transcriptformer/lymphnode_tsv2.h5ad",
+    force_download: bool = False,
+    **kwargs: Any,
+) -> ad.AnnData:
+    """
+    Lymph node dataset from Tabula Sapiens v2.
+    """
+    return _load_dataset_from_url(
+        path,
+        file_type="h5ad",
+        backup_url="https://figshare.com/ndownloader/files/40569779",
+        expected_shape=(54134, 2000),
+        force_download=force_download,
+        **kwargs,
+    )
+
+
+def _load_dataset_from_url(
+    fpath: PathLike,
+    file_type: Literal["h5ad"],
+    *,
+    backup_url: str,
+    expected_shape: Tuple[int, int],
+    force_download: bool = False,
+    **kwargs: Any,
+) -> ad.AnnData:
+    fpath = os.path.expanduser(fpath)
+    assert file_type in ["h5ad"], f"Invalid type `{file_type}`. Must be one of `['h5ad']`."
+    if not fpath.endswith(file_type):
+        fpath += f".{file_type}"
+    if force_download and os.path.exists(fpath):
+        os.remove(fpath)
+    if not _check_datafile_present_and_download(backup_url=backup_url, path=fpath):
+        raise FileNotFoundError(f"File `{fpath}` not found or download failed.")
+    data = ad.read_h5ad(filename=fpath, **kwargs)
+
+    if data.shape != expected_shape:
+        raise ValueError(f"Expected AnnData object to have shape `{expected_shape}`, found `{data.shape}`.")
+
+    return data
