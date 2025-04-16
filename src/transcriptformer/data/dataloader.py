95c320c96c735083e0a02f2db8ec25503b29f0e8
flow <flow@chanzuckerberg.com>
2025-04-16 09:31:40 -0700

    update

--- src/transcriptformer/data/dataloader.py
+++ src/transcriptformer/data/dataloader.py
@@ -47,8 +47,6 @@ def apply_filters(
     min_expressed_genes,
 ):
     """Apply filters to the data."""
-    print(vocab)
-    print(gene_names)
     if filter_to_vocab:
         filter_idx = [i for i, name in enumerate(gene_names) if name in vocab]
         X = X[:, filter_idx]
@@ -234,7 +232,6 @@ class AnnDataset(Dataset):
             return None
 
         gene_names, success = load_gene_features(adata, self.gene_col_name)
-        print(gene_names)
         if not success:
             logging.error(f"Failed to load gene features from {file_path}")
             return None
@@ -256,8 +253,6 @@ class AnnDataset(Dataset):
         if X is None:
             logging.warning(f"Data was filtered out completely for {file_path}")
             return None
-        
-        print(X.shape, obs.shape)
 
         batch = process_batch(
             X,
